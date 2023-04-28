761b8078c71dd3db8f321a983a5b494c4d073439
LNUAKX <lukmaannadaf@gmail.com>
2023-04-28 21:59:38 +0530

    added some new papes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 require("dotenv").config();
-
+// const ejs=require("ejs");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
@@ -10,6 +10,13 @@ app.use(express.static(__dirname+'/public'));
 
 app.get("/",(req,res)=>{
     res.render('home');
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+app.get("/register",(req,res)=>{
+    res.render("register");
 })
 
 app.post("/login",(req,res)=>{
