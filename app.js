ca5d3f001133311762225baeb12474bad1071ea5
LNUAKX <lukmaannadaf@gmail.com>
2023-04-28 18:23:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+require("dotenv").config();
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static(__dirname+'/public'));
+
+
+app.get("/",(req,res)=>{
+    res.render('home');
+})
+
+
+const port=process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log("server started at "+port);
+})
