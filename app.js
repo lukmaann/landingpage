d0adcb419fd8a539f4e7607a42e45f420b8fb79d
LNUAKX <lukmaannadaf@gmail.com>
2023-04-29 11:18:20 +0530

    added mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 // const ejs=require("ejs");
 const app=express();
@@ -7,7 +8,27 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
 
+// -----------------------------database-----------------------------------
 
+const uri=process.env.URI;
+mongoose.connect(uri).then(()=>{
+    console.log("connected to mongodb");
+})
+const userschema=new mongoose.Schema({
+    email:String,
+    password:String
+    
+})
+
+const newuser=mongoose.model("user",userschema);
+
+
+
+
+
+
+
+// ----------------------------------------------------------------------------
 app.get("/",(req,res)=>{
     res.render('home');
 })
@@ -19,8 +40,41 @@ app.get("/register",(req,res)=>{
     res.render("register");
 })
 
+
+
 app.post("/login",(req,res)=>{
-    res.send("welcome")
+    const email=req.body.email;
+    const pass=req.body.password;
+    newuser.findOne({email:email}).then((found)=>{
+        if(!found){
+            res.send("user not register please register")
+        }else{
+            if(pass===found.password){
+                res.send("welcome")
+            }else{
+                res.send("getlost you mf")
+            }
+        }
+    })
+    
+})
+
+app.post("/register",(req,res)=>{
+    const email=req.body.email;
+    const pass=req.body.password;
+    newuser.findOne({email:email}).then((found)=>{
+        if(!found){
+            const register=new newuser({email:email,password:pass});
+            register.save().then(()=>{
+                res.send("new user registered");
+            })
+            
+        }else{
+            res.send("user already regitered");
+        }
+    })
+   
+
 })
 const port=process.env.PORT || 3000;
 
